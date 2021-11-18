8c6698cd767cda965f7d00cb9700c0e03b542799
KauaneDelvoss <86755715+KauaneDelvoss@users.noreply.github.com>
2021-11-18 16:51:39 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Conversor binário para decimal</h1>
+    <Conversor />
+    
+  </div>
+</template>
+
+<script>
+
+import Conversor from '@/components/Conversor'
+export default {
+  name: 'App',
+  components: 
+    { Conversor }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="principal">
+    <div class="entradaDados">
+      <label for="entradaBinario">valor binario</label>
+      <input @input="adicionarBit" name="entradaBinario" type="text">
+    </div>
+    <button @click="converter" class="btConversor">Converter</button>
+    <div class="saidaDados">
+      <label for="saidaDecimal">Valor decimal</label>
+      <input v-model="valorDecimal" disabled name="saidaDecimal" type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valorBinario: '',
+      valorDecimal: ''
+    }
+  },
+  methods: {
+    bin2dec(valor) {
+      const valorInvertido = valor.split("").reverse();
+      let total = 0, pos = 0;
+      for (const bit of valorInvertido) {
+        total += bit * 2 ** pos;
+        pos++;
+      }
+      return total;
+    },
+    converter() {
+      this.valorDecimal = this.bin2dec(this.valorBinario)
+    },
+    adicionarBit(e){
+          let erro = false
+          for (const bit of e.target.value) {
+              if (bit!=='0' && bit!== '1') {
+                  erro = true
+              }
+          }
+          if (erro) {
+              e.target.value = this.valorBinario
+          }
+          this.valorBinario = e.target.value
+      }
+
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
